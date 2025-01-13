e3f0b1331e7054ea7e7f23a6aefc3633517b3409
GioelePalese <g.palese@viastore.com>
2025-01-13 14:38:21 +0100

    The different arguments now are given through flags instead of being asked by the program.

--- Script for Thorsten.py
+++ Script for Thorsten.py
@@ -1,10 +1,26 @@
 import os
+import argparse
 import xml.etree.ElementTree as ET
 import csv
 
-# Ask for the folder path where data has to be read from and the path for the file where everything will be saved
-folder_path = input ( "Folder Path: " )
-output_csv_path  = input ( "CSV Path: " )
+# Define all arguments available
+parser = argparse.ArgumentParser()
+parser.add_argument("-fp", "--folder_path", help="The folder containing the XML files.", required=True)
+parser.add_argument("-op", "--output_file", help="The CSV file where all data will be stored.", required=True)
+args = parser.parse_args()
+
+# Get all arguments
+folder_path = args.folder_path
+output_file = args.output_file
+
+# Check if all required arguments were given
+if folder_path is None or output_file is None:
+    print("Not enough data was given, use -h to see all arguments.")
+    exit()
+
+# Format output_file so that it ends with .csv
+if not output_file.endswith('.csv'):
+    output_file += '.csv'
 
 # Get all files that end with .dms1importbatch from the folder given
 files = [f for f in os.listdir(folder_path) if f.endswith('.dms1importbatch')]
@@ -33,7 +49,7 @@ for file_name in files:
     rows.append(row)
 
 # Write all data collected in the given CSV file
-with open(output_csv_path, 'w', newline = '', encoding = "utf-8") as csvfile:
+with open(output_file, 'w', newline = '', encoding = "utf-8") as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames = headers, delimiter = ';')
     writer.writeheader()
     for row in rows:
